08915078df79cf5468caa31f95db85914bf89d08
KupDanl <mrgandghubas@gmail.com>
2018-05-02 14:02:23 +0300

    upd14

--- 3/index.js
+++ 3/index.js
@@ -6,11 +6,11 @@ request("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3", func
 	var courses = "";
 	var obj = JSON.parse(body);
 	var data = fs.readFileSync("courses.html", "utf8");
-	for(i = 0; i < obj.length; i++){
-      var rate = obj[i].ccy + " : Buy : " + obj[i].buy + " > Sale : " + obj[i].sale;
-      str = JSON.stringify(rate).replace(/\"/g, "");
-      courses += str + "\n";
-    }  
+	for(var i = 0; i < obj.length; i++){
+    	var rate = obj[i].ccy + " > Buy: " + obj[i].buy + ". Sale: " + obj[i].sale + ".";
+     	str = JSON.stringify(rate).replace(/\"/g, "");
+    	courses += str + "\n";
+    }
 
     fs.writeFile("courses.html", courses, function (err) {
 		if(err){
